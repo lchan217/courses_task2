6f341ba20117f3724bd2fb97f10427f8be2ef737
Lizzie <lchan217@gmail.com>
2020-04-12 12:02:14 -0700

    add initial show page layout

--- src/components/ShowCourse.js
+++ src/components/ShowCourse.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Image } from "react-bootstrap";
 
-const ShowCourse = () => {
-  return <div>Single Course Page</div>;
+const ShowCourse = props => {
+  const {
+    title,
+    author,
+    shortDescription,
+    duration,
+    durationPeriod,
+    imgUrl,
+    level,
+    url
+  } = props.location.state.course._source;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h3>By: {author}</h3>
+      <Image src={imgUrl} style={{ width: "20em" }} />
+      <div>{shortDescription}</div>
+      <a href={url}>Website</a>
+      <div>
+        Time: {duration} {durationPeriod}
+      </div>
+      <div>Level: {level}</div>
+    </div>
+  );
 };
 
 export default ShowCourse;
