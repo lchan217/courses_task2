fa5aa26c574b9a2a91b26cdaba872f8b0a760ef3
Lizzie <lchan217@gmail.com>
2020-04-12 11:00:41 -0700

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import CoursesContainer from "./components/CoursesContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
