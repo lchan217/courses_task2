0da833cd894a8f4f92461d418d4e900af85b7a02
Lizzie <lchan217@gmail.com>
2020-04-12 11:31:27 -0700

    add initial card layout

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
+import { Card, Button } from "react-bootstrap";
 
 class ResultList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showCourse: false,
+      course: {}
+    };
+  }
+
   render() {
-    return <div>Result list</div>;
+    let data;
+    if (this.state.showCourse) {
+      data = "Course Page";
+    } else {
+      data = this.props.courses.map(course => {
+        return (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img src={`${course._source.imgUrl}`} />
+            <Card.Body>
+              <Card.Title>{course._source.title}</Card.Title>
+              <Card.Text>
+                {course._source.shortDescription}.slice(0,300)
+              </Card.Text>
+              <Button>More Details</Button>
+            </Card.Body>
+            <Card.Footer>
+              {course._source.quzeTags &&
+                course._source.quzeTags.split(",").map(tag => {
+                  return (
+                    <Button size='sm' variant='secondary'>
+                      {tag}{" "}
+                    </Button>
+                  );
+                })}
+            </Card.Footer>
+          </Card>
+        );
+      });
+    }
+
+    return <div>{data}</div>;
   }
 }
 
